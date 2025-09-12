a76cdc2d3cb86a712eb5e10ed234e73cd243fddb
Rohit Singh <rr2488017@gmail.com>
2025-09-12 17:37:57 +0530

    More Trie Questions

---
+++ Trie/LongestWord.java
@@ -0,0 +1,68 @@
+public class LongestWord {
+
+    static class Node {
+        Node[] children = new Node[26];
+        boolean eow = false;
+
+        public Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+
+        for (char ch : word.toCharArray()) {
+            int idx = ch - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
+
+    static StringBuilder ans = new StringBuilder();
+
+    public static void longestWord(Node root, StringBuilder temp) {
+
+        if(root == null) {
+            return;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (root.children[i] != null && root.children[i].eow == true) {
+                char ch = (char) (i + 'a');
+                temp.append(ch);
+
+                if (temp.length() > ans.length()) {
+                    ans = new StringBuilder();
+                    ans.append(temp);
+                }
+
+                longestWord(root.children[i], temp);
+
+                temp.deleteCharAt(temp.length() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String words[] = { "a", "banana", "app", "appl", "ap", "apply", "apple" };
+
+        for (String word : words) {
+            insert(word);
+        }
+
+        longestWord(root, new StringBuilder());
+
+        System.out.println(ans);
+
+    }
+}
