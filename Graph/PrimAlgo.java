5835d08d9be54bb60382c968d558fb07c265915d
Rohit Singh <rr2488017@gmail.com>
2026-03-14 10:41:13 +0530

    Graph Algorithms

---
+++ Graph/PrimAlgo.java
@@ -0,0 +1,15 @@
+public class PrimAlgo {
+    public static void main(String[] args) {
+        int[][] edges = {
+                { 0, 2, 2 },
+                { 0, 1, 6 },
+                { 0, 3, 4 },
+                { 3, 1, 1 },
+                { 2, 1, 3 },
+                { 2, 4, 1 },
+                { 4, 1, 1 },
+        };
+
+        int n = 5;
+    }
+}
